c938b347a81a25ae78119eb93373ef29ac45a2de
mcast045 <mcastaneda42@gmail.com>
2020-06-16 14:10:04 -0700

    Video Preload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+let video = document.querySelector('.landing-video__content')
+if (video) {
+    video.src = 'my_video_' + value + '.ogg';
+    video.load();
+}
+
+
 window.Reveal = ScrollReveal()
-
 Reveal.reveal('.zoomIn', { duration: 1500, scale: 0 })
-
 Reveal.reveal('.left-reveal', { distance: '400px', duration: 1500, origin: 'left' })
 Reveal.reveal('.bottom-reveal', { distance: '400px', duration: 1500, origin: 'bottom' })
 Reveal.reveal('.right-reveal', { distance: '400px', duration: 1500, origin: 'right' })
