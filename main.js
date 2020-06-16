18601b7266020b0fc284bf7e05b16e9797d03521
mcast045 <mcastaneda42@gmail.com>
2020-06-16 14:00:43 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+window.Reveal = ScrollReveal()
+
+Reveal.reveal('.zoomIn', { duration: 1500, scale: 0 })
+
+Reveal.reveal('.left-reveal', { distance: '400px', duration: 1500, origin: 'left' })
+Reveal.reveal('.bottom-reveal', { distance: '400px', duration: 1500, origin: 'bottom' })
+Reveal.reveal('.right-reveal', { distance: '400px', duration: 1500, origin: 'right' })
