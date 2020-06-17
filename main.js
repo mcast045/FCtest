ec06c0f4f924f1b6350314b7fc3e9e1e936d9baf
mcast045 <mcastaneda42@gmail.com>
2020-06-17 13:14:28 -0700

    Video preload

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 let video = document.querySelector('.landing-video__content')
 
-video.addEventListener('loadeddata', function () {
-    // Video is loaded and can be played
-}, false);
-
 
 window.Reveal = ScrollReveal()
 Reveal.reveal('.zoomIn', { duration: 1500, scale: 0 })
