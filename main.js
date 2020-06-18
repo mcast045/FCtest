b53ce1179c49ab09c8ee8af5e81f73f500770c83
mcast045 <mcastaneda42@gmail.com>
2020-06-18 13:28:26 -0700

    Services Update

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let video = document.querySelector('.landing-video__content')
-
-
 window.Reveal = ScrollReveal()
+
 Reveal.reveal('.zoomIn', { duration: 1500, scale: 0 })
-Reveal.reveal('.left-reveal', { distance: '400px', duration: 1500, origin: 'left' })
-Reveal.reveal('.bottom-reveal', { distance: '400px', duration: 1500, origin: 'bottom' })
-Reveal.reveal('.right-reveal', { distance: '400px', duration: 1500, origin: 'right' })
+
+if (window.screen.width > 999) {
+    Reveal.reveal('.left-reveal', { distance: '400px', duration: 1500, origin: 'left' })
+    Reveal.reveal('.bottom-reveal', { distance: '400px', duration: 1500, origin: 'bottom' })
+    Reveal.reveal('.right-reveal', { distance: '400px', duration: 1500, origin: 'right' })
+}
